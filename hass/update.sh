b8f3897919f3ebf6435ae9c18a83b1609230b1ba
patuan81 <pa.tuan@gmail.com>
2019-05-29 14:38:16 +0700

    fix

--- hass/update.sh
+++ hass/update.sh
@@ -3,7 +3,7 @@
 docker pull homeassistant/raspberrypi3-homeassistant
 #x86
 #docker pull homeassistant/home-assistant
-docker stop ha
-docker rm ha
+# docker stop hass
+# docker rm hass
 docker-compose up -d
 docker image prune -f
